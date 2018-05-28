c0c337b693f9c8883e0b783f09d1a2b1f3e17482
Marco Parenzan <marco.parenzan@live.it>
2018-05-28 09:20:02 +0200

    Add project files.

---
+++ IoTHubEvents/dbo/Tables/Averages.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Averages]
+(
+	[Id] BIGINT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	DeviceId NVARCHAR(64) NOT NULL,
+	[Timestamp] datetimeoffset(7) NOT NULL,
+	Value decimal(18,3) NOT NULL, 
+    [Count] INT NULL
+)
---
+++ IoTHubMonitoring/Controllers/DevicesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IoTHubMonitoring.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Devices;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace IoTHubMonitoring.Controllers
+{
+    public class DevicesController : Controller
+    {
+        private IConfiguration _configuration;
+
+        public DevicesController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var rm = RegistryManager.CreateFromConnectionString(_configuration["IoTHubConnectionString"]);
+
+            var devicesQuery = rm.CreateQuery("SELECT DeviceId FROM devices");
+            var devices = await devicesQuery.GetNextAsJsonAsync();
+
+            var list = new List<DeviceListDto>();
+
+            foreach (var device in devices)
+            {
+                var json = JsonConvert.DeserializeObject<JObject>(device);
+                var dto = new DeviceListDto
+                {
+                    DeviceId = json.Value<string>("DeviceId")
+                };
+                list.Add(dto);
+            }
+
+            return View(list);
+        }
+    }
+}
